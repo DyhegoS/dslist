d6a1a44489c1734a3ec7d947f58350751cdd6040
DyhegoS <dyhegodasilva@gmail.com>
2025-05-20 21:49:06 -0300

    Consulta SQL, projection, busca de games por lista

--- src/main/java/com/giministorm/dslist/services/GameService.java
+++ src/main/java/com/giministorm/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.giministorm.dslist.dto.GameDTO;
 import com.giministorm.dslist.dto.GameMinDTO;
 import com.giministorm.dslist.entities.Game;
+import com.giministorm.dslist.projections.GameMinProjection;
 import com.giministorm.dslist.repositories.GameRepository;
 
 
@@ -27,6 +28,12 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
