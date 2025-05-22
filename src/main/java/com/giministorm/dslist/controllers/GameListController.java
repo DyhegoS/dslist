ffae41bdd64312a891bf10a8b35d7cec59c01f40
DyhegoS <dyhegodasilva@gmail.com>
2025-05-22 11:10:48 -0300

    Adicionado função de mover jogos no banco de dados

--- src/main/java/com/giministorm/dslist/controllers/GameListController.java
+++ src/main/java/com/giministorm/dslist/controllers/GameListController.java
@@ -5,23 +5,33 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.giministorm.dslist.dto.GameListDTO;
 import com.giministorm.dslist.dto.GameMinDTO;
+import com.giministorm.dslist.dto.ReplacementDTO;
+import com.giministorm.dslist.repositories.GameListRepository;
 import com.giministorm.dslist.services.GameListService;
 import com.giministorm.dslist.services.GameService;
 
 @RestController
 @RequestMapping(value = "/lists")
 public class GameListController {
+
+    private final GameListRepository gameListRepository;
 	
 	@Autowired
 	private GameListService gameListService;
 	
 	@Autowired
 	private GameService gameService;
+
+    GameListController(GameListRepository gameListRepository) {
+        this.gameListRepository = gameListRepository;
+    }
 	
 	@GetMapping
 	public List<GameListDTO> findAll(){
@@ -33,5 +43,10 @@ public class GameListController {
 	public List<GameMinDTO> findByList(@PathVariable Long listId){
 		List<GameMinDTO> result = gameService.findByList(listId);
 		return result;
+	}
+	
+	@PostMapping(value = "{listId}/replacement")
+	public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body){
+		gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
 	}
 }
